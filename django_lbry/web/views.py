4f6cbdaf3e68f039f04f5e3d901691fc43678a15
lpessin <luizpessin@hotmail.com>
2020-05-20 00:07:23 -0300

    setting only streams on today's top check if is channel on claim-grid.html added

--- django_lbry/web/views.py
+++ django_lbry/web/views.py
@@ -7,9 +7,9 @@ def home(request):
     trending = sdk.claim_search(5, claim_type='stream', order_by=['support_amount'],
                                 any_tags=['python', 'technology', 'linux'], page_size=8, page=1)
 
-    channels = sdk.claim_search(2, claim_type='channel', order_by=['support_amount'], page_size=4, page=1)
+    channels = sdk.claim_search(5, claim_type='channel', order_by=['support_amount'], page_size=4, page=1)
 
-    today = sdk.claim_search(1, order_by=['support_amount'], page_size=4, page=1)
+    today = sdk.claim_search(1, claim_type='stream', order_by=['support_amount'], page_size=4, page=1)
 
     lbry = sdk.claim_search(claim_type='stream', channel_ids=['f8219d5914b95a0a2f670d92dea5dc24133278e9'],
                             order_by=['release_time'], page_size=4, page=1)
